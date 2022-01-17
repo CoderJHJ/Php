485fc3ad1d2f7b888dcf805424180840ebb16eb2
Jahid Hasan Jewel <94279695+CoderJHJ@users.noreply.github.com>
2022-01-17 22:44:40 +0600

    Update home.php

--- Batch 01/home.php
+++ Batch 01/home.php
@@ -1,33 +1,33 @@
 <?php
-//$f_name= 'Jewel';
-//$l_name= 'khan';
-//$_COOKIE= 'My first code for php';
-//$_POST= 'Welcome Php';
-//echo $f_name.' '.$l_name."  ".$_COOKIE.'  '.$_POST;
-// $myname= 'Hello Bangladesh';
-// echo strlen($myname);
-// echo strrev($myname);
+$f_name= 'Jewel';
+$l_name= 'khan';
+$_COOKIE= 'My first code for php';
+$_POST= 'Welcome Php';
+echo $f_name.' '.$l_name."  ".$_COOKIE.'  '.$_POST;
+ $myname= 'Hello Bangladesh';
+ echo strlen($myname);
+ echo strrev($myname);
 //uporer code hocce lekhake reverse kora and length check kora
-// $std1 = 'Jewel';
-// $std2 ='Jewel';
-// $std3 ='jewel';
-// echo $std1. '<br>';
-// echo $std2;
-// echo '<br>';
-// echo $std3;
-//lekhar maje break deeyar jonno <br> use kora jay
-/*$num1 =10;
+$std1 = 'Jewel';
+ $std2 ='Jewel';
+ $std3 ='jewel';
+echo $std1. '<br>';
+echo $std2;
+echo '<br>';
+ echo $std3;
+lekhar maje break deeyar jonno <br> use kora jay
+$num1 =10;
 $num2 =5;
 echo $num1*$num2. '<b>';
 echo$num1/$num2.'<b>';
-echo$num1**$num2.'<b>';*/
-//operator er bebohar
-// $numb1 = 33;
-// $nunb2= 44;
-// echo $numb<$nunb2;
-// if($numb1<$nunb2){
-//     echo 'True';
-// }
+echo$num1**$num2.'<b>';
+operator er bebohar
+ $numb1 = 33;
+ $nunb2= 44;
+ echo $numb<$nunb2;
+ if($numb1<$nunb2){
+     echo 'True';
+ }
 $myvalu = 45.33;
 echo round($myvalu);
 echo (int)$myvalu;
